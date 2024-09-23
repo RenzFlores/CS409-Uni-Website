9e778a0d35755affeda21d1b8dc77d213425e096
RenzFlores <renztomagan12@gmail.com>
2024-09-24 01:25:50 +0800

    update

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const form = document.getElementById('form');
+const name = document.getElementById('name');
+const birthdate = document.getElementById('birthdate');
+const contactnum = document.getElementById('contactnum');
+const lastschool = document.getElementById('lastschool');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submitReg', e => {
 	e.preventDefault();
 	
-	checkInputs();
+	checkRegInputs();
 });
 
-function checkInputs() {
+form.addEventListener('submitLogin', e => {
+	e.preventDefault();
+	
+	checkLoginInputs();
+});
+
+function checkRegInputs() {
 	// trim to remove the whitespaces
+    const nameValue = name.value.trim();
+    const birthdateValue = birthdate.value.trim();
+    const contactnumValue = contactnum.value.trim();
+    const lastschoolValue = lastschool.value.trim();
 	const usernameValue = username.value.trim();
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
@@ -46,6 +60,19 @@ function checkInputs() {
 	}
 }
 
+function checkLoginInputs() {
+    const usernameValue = username.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+
+    // TODO: Check for matching existing records.
+    if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+}
+
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
 	const small = formControl.querySelector('small');
@@ -61,17 +88,3 @@ function setSuccessFor(input) {
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
-
-
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
