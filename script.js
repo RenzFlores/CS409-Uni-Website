376cd0b3273dc58be35683fff9ea25776b8cc8c0
RenzFlores <renztomagan12@gmail.com>
2024-09-26 08:14:39 +0800

    update
    bug: authenticateLogin() is asynchronous, leading to undefined regardless if a user with matching credentials is found

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const form = document.getElementById('form');
+const loginform = document.getElementById('loginform');
+const regform = document.getElementById('regform');
 const name = document.getElementById('name');
 const birthdate = document.getElementById('birthdate');
 const contactnum = document.getElementById('contactnum');
@@ -8,17 +9,153 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-form.addEventListener('submitReg', e => {
-	e.preventDefault();
-	
-	checkRegInputs();
-});
+let db;
+const request = indexedDB.open("UserDatabase", 1);
 
-form.addEventListener('submitLogin', e => {
-	e.preventDefault();
-	
-	checkLoginInputs();
-});
+var currentUser = null;
+
+request.onupgradeneeded = function(event) {
+    db = event.target.result;
+    // Create an object store for this database if it doesn't already exist
+    const objectStore = db.createObjectStore("Users", { keyPath: "id", autoIncrement: true});
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    console.log("Database opened successfully");
+};
+
+request.onerror = function(event) {
+    console.error("Database error: ", event.target.errorCode);
+};
+
+var userObject = {
+    name: "",
+    birthdate: "",
+    contactnum: "",
+    lastschool: "",
+    username: "",
+    email: "",
+    password: ""
+}
+
+try {
+    regform.addEventListener('submit', e => {
+        e.preventDefault();
+    
+        if (checkRegInputs()) {
+            addUser();
+        }
+    });
+}
+catch(err) {
+    ;
+}
+
+try {
+    loginform.addEventListener('submit', e => {
+        e.preventDefault();
+
+        if (checkLoginInputs()) {
+            result = authenticateLogin();
+            console.log(result);
+            if (result !== undefined) {
+                window.alert("Welcome " + (result.name) + "!");
+                currentUser = result;
+            } else {
+                window.alert("Incorrect username or password.");
+            }
+        }
+    });
+}
+catch(err) {
+    ;
+}
+
+function addUser() {
+    const newUser = {
+        name: "",
+        birthdate: "",
+        contactnum: "",
+        lastschool: "",
+        username: "",
+        email: "",
+        password: ""
+    };
+
+    newUser.name = name.value.trim();
+    newUser.birthdate = birthdate.value.trim();
+    newUser.contactnum = contactnum.value.trim();
+    newUser.lastschool = lastschool.value.trim();
+    newUser.username = username.value.trim();
+    newUser.email = email.value.trim();
+    newUser.password = password.value.trim();
+    saveUser(newUser);
+    console.log("User successfully added.");
+}
+
+function saveUser(userObject) {
+    const transaction = db.transaction(["Users"], "readwrite");
+    const objectStore = transaction.objectStore("Users");
+
+    /*
+    try {
+        const request = objectStore.put(userObject);
+    } catch(DataError) {
+        const request = objectStore.put({name: "test", password: "testpass", id: 1});
+    }
+    */
+
+    const request = objectStore.put(userObject);
+
+    request.onsuccess = function() {
+        console.log("Object saved successfully");
+    };
+
+    request.onerror = function() {
+        console.error("Error saving object");
+    };
+}
+
+function authenticateLogin() {
+    let transaction = db.transaction(["Users"], "readonly"); 
+    let users = transaction.objectStore("Users");
+    let request = users.getAll();
+
+    request.onsuccess = function() {
+        if (request.result !== undefined) { 
+            for (var i = 0; i < request.result.length; i++) {
+                if (matchRecords(request.result[i])) {
+                    console.log("User found");
+                    return request.result[i];    // User is found
+                }
+            }
+            console.log("User not found");
+            return null;
+        } else { 
+            console.log("User database is empty.");
+            return null;
+        }
+    };
+
+    function matchRecords(user) {
+        if (user.username.localeCompare(document.getElementById("username").value) === 0) {
+            //console.log("Username matched.");
+        } else { 
+            //console.log("Username not found.");
+            return false;
+        }
+
+        if (user.password.localeCompare(document.getElementById("password").value) === 0) {
+            //console.log("Password matched.");
+        } else { 
+            //console.log("Password not found.");
+            return false;
+        }
+
+        return true;
+    }
+}
 
 function checkRegInputs() {
 	// trim to remove the whitespaces
@@ -30,47 +167,103 @@ function checkRegInputs() {
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
 	const password2Value = password2.value.trim();
+    var isValid = true;
 	
-	if(usernameValue === '') {
-		setErrorFor(username, 'Username cannot be blank');
+    if(nameValue === '') {
+		setErrorFor(name, 'Required');
+        isValid = false;
 	} else {
-		setSuccessFor(username);
-	}
+        revertForm(name);
+    }
+
+    if(birthdateValue === '') {
+		setErrorFor(birthdate, 'Required');
+        isValid = false;
+	} else {
+        revertForm(birthdate);
+    }
+
+	if(contactnumValue === '') {
+		setErrorFor(contactnum, 'Required');
+        isValid = false;
+	} else {
+        revertForm(contactnum);
+    }
+
+    if(lastschoolValue === '') {
+		setErrorFor(lastschool, 'Required');
+        isValid = false;
+	} else {
+        revertForm(lastschool);
+    }
+
+    if(usernameValue === '') {
+		setErrorFor(username, 'Required');
+        isValid = false;
+	} else {
+        revertForm(username);
+    }
 	
 	if(emailValue === '') {
-		setErrorFor(email, 'Email cannot be blank');
+		setErrorFor(email, 'Required');
+        isValid = false;
 	} else if (!isEmail(emailValue)) {
 		setErrorFor(email, 'Not a valid email');
-	} else {
-		setSuccessFor(email);
-	}
+        isValid = false;
+    } else {
+        revertForm(email);
+    }
 	
 	if(passwordValue === '') {
-		setErrorFor(password, 'Password cannot be blank');
+		setErrorFor(password, 'Required');
+        isValid = false;
 	} else {
-		setSuccessFor(password);
-	}
+        revertForm(password);
+    }
 	
 	if(password2Value === '') {
-		setErrorFor(password2, 'Password2 cannot be blank');
+		setErrorFor(password2, 'Required');
+        isValid = false;
 	} else if(passwordValue !== password2Value) {
 		setErrorFor(password2, 'Passwords does not match');
-	} else{
-		setSuccessFor(password2);
-	}
+        isValid = false;
+	} else {
+        revertForm(password2);
+    }
+
+    if (isValid) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function checkLoginInputs() {
     const usernameValue = username.value.trim();
 	const passwordValue = password.value.trim();
-	const password2Value = password2.value.trim();
+
+    let isValid = true;
 
     // TODO: Check for matching existing records.
     if(usernameValue === '') {
 		setErrorFor(username, 'Username cannot be blank');
+        isValid = false;
 	} else {
-		setSuccessFor(username);
-	}
+        revertForm(username);
+    }
+
+    if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+        isValid = false;
+	} else {
+        revertForm(password);
+    }
+
+    if (isValid) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function setErrorFor(input, message) {
@@ -80,9 +273,11 @@ function setErrorFor(input, message) {
 	small.innerText = message;
 }
 
-function setSuccessFor(input) {
+function revertForm(input) {
 	const formControl = input.parentElement;
-	formControl.className = 'form-control success';
+	const small = formControl.querySelector("small");
+	formControl.className = "form-control";
+	small.innerText = "";
 }
 	
 function isEmail(email) {
